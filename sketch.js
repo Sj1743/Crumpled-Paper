6ad732fe73f9619ec380cf0774ae2ad137001692
Sj1743 <68275266+Sj1743@users.noreply.github.com>
2020-08-09 16:17:18 +0530

    PRO - C25

--- sketch.js
+++ sketch.js
@@ -5,54 +5,61 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var ground;
+var dustbin, dustbinImage;
 var boxBottom, boxLeft, boxRight;
 var crumpledPaper;
+
+
+function preload(){
+	dustbinImage = loadImage("dustbin.png");
+}
 
 
 function setup() {
 	createCanvas(800, 700);
 
-
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
 	ground = new Ground(400, height, width, 10);
 	crumpledPaper = new Paper();
 
-	boxBottom = Bodies.rectangle(650, 690, 200, 10, {isStatic:true});
+	boxBottom = Bodies.rectangle(700, 690, 100, 10, {isStatic:true});
 	World.add(world, boxBottom);
-	boxLeft = Bodies.rectangle(555, 640, 10, 100, {isStatic:true});
+	boxLeft = Bodies.rectangle(655, 625, 10, 125, {isStatic:true});
 	World.add(world, boxLeft);
-	boxRight = Bodies.rectangle(745, 640, 10, 100, {isStatic:true});
+	boxRight = Bodies.rectangle(745, 625, 10, 125, {isStatic:true});
 	World.add(world, boxRight);
 
-	//create sprites for 'box'
-	boxBottomBody = createSprite(650, 690, 200, 10);
+	boxBottomBody = createSprite(700, 690, 100, 10);
 	boxBottomBody.shapeColor = "white";
-	boxLeftBody = createSprite(555, 640, 10, 100);
+	boxLeftBody = createSprite(655, 625, 10, 125);
 	boxLeftBody.shapeColor = "white";
-	boxRightBody = createSprite(745, 640, 10, 100);
+	boxRightBody = createSprite(745, 625, 10, 125);
 	boxRightBody.shapeColor = "white";
 
+	dustbin = createSprite(700, 630, 0, 0);
+	dustbin.addImage(dustbinImage);
+	dustbin.scale = 0.42;	
+
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background("grey");
+
+  drawSprites();
 
   ground.display();
   crumpledPaper.display();
   
-  drawSprites();
- 
 }
+
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(crumpledPaper.body, crumpledPaper.body.position, {x: 20, y:-12});
+		Matter.Body.applyForce(crumpledPaper.body, crumpledPaper.body.position, {x: 60, y:-55});
 	}
 }
